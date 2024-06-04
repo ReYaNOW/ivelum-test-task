3f497dbfd8216e0ecf7c12f420eab0a1df4237ae
Sergei <reyangood@gmail.com>
2024-06-04 18:14:52 +0300

    make requests via aiohttp

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 
+import aiohttp
 from bs4 import BeautifulSoup
-import requests
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 
@@ -10,16 +10,27 @@ app = FastAPI()
 URL = 'https://news.ycombinator.com/'
 
 
-def change_page_body(response):
-    """adding to all six-letter words ™ symbols"""
-    soup = BeautifulSoup(response.text, 'html.parser')
+@app.get('/{path:path}')
+async def serve_my_app(request: Request, path=''):
+    query = f'?{request.query_params}' if request.query_params else ''
+    response = await make_request(f'{URL}{path}{query}')
+    changed_body = change_page_body(response)
+    return HTMLResponse(content=changed_body, status_code=200)
 
-    for elem in soup.find_all(
-        string=re.compile(r'.*(^|\s)\b(\w{6})\b($|\s)[\s\S]*')
-    ):
-        elem.string.replace_with(
-            re.sub(r'\b(\w{6})\b', r'\1' + '™', elem.string)
-        )
+
+async def make_request(final_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(final_url) as response:
+            return await response.read()
+
+
+def change_page_body(text):
+    """adding to all six-letter words ™ symbols"""
+    soup = BeautifulSoup(text, 'html.parser')
+
+    for elem in soup.find_all(string=re.compile(r'\b\w{6}\b')):
+        elem.replace_with(re.sub(r'\b(\w{6})\b', r'\1™', elem))
+
     for elem in soup.find_all(['img', 'link', 'script']):
         if elem.get('src'):
             elem['src'] = URL + elem['src']
@@ -27,11 +38,3 @@ def change_page_body(response):
             elem['href'] = URL + elem['href']
 
     return soup.prettify()
-
-
-@app.get('/{path:path}')
-async def serve_my_app(request: Request, path=''):
-    query = f'?{request.query_params}' if request.query_params else ''
-    response = requests.get(f'{URL}{path}{query}')
-    changed_body = change_page_body(response)
-    return HTMLResponse(content=changed_body, status_code=200)
